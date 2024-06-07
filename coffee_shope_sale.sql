fe20a5ec67e6a5afedcdea609fdd72a8dde25e62
Yashbahuguna31 <149990665+Yashbahuguna31@users.noreply.github.com>
2024-06-07 16:31:41 +0530

    Update coffee_shope_sale.sql

--- coffee_shope_sale.sql
+++ coffee_shope_sale.sql
@@ -11,17 +11,17 @@ from coffee_sales
 -- TOTAL SALES KPI - MOM DIFFERENCE AND MOM GROWTH
 
 with cte as (
-				select to_char(transaction_date,'Mon') as months,extract(month from transaction_date ) as month_number
-					   ,round(sum(transaction_qty*unit_price),2) as sales
-				from coffee_sales
-				group by to_char(transaction_date,'Mon'),extract(month from transaction_date )
-				order by month_number asc
+	     select to_char(transaction_date,'Mon') as months,extract(month from transaction_date ) as month_number
+		    ,round(sum(transaction_qty*unit_price),2) as sales
+	     from coffee_sales
+	     group by to_char(transaction_date,'Mon'),extract(month from transaction_date )
+	    order by month_number asc
 	        )
 select months,sales,prv_month_sales,
        (sales - prv_month_sales) as diff_sales,
 	   round(((sales - prv_month_sales)/prv_month_sales)*100,2) as percentage_change
 from (
-	   select *,
+      select *,
              lag(sales) over(order by month_number asc) prv_month_sales
        from cte
 	 ) x
@@ -34,17 +34,17 @@ from coffee_sales
 -- TOTAL ORDERS KPI - MOM DIFFERENCE AND MOM GROWTH
 
 with cte as (
-				select to_char(transaction_date,'Mon') as months,extract(month from transaction_date ) as month_number
-					   ,count(transaction_qty) as orders
-				from coffee_sales
-				group by to_char(transaction_date,'Mon'),extract(month from transaction_date )
-				order by month_number asc
+	      select to_char(transaction_date,'Mon') as months,extract(month from transaction_date ) as month_number
+		     ,count(transaction_qty) as orders
+	      from coffee_sales
+	      group by to_char(transaction_date,'Mon'),extract(month from transaction_date )
+	      order by month_number asc
 	        )
 select months,orders,prv_month_orders,
        (orders - prv_month_orders) as diff_orders,
 	   round((orders - prv_month_orders)*100.0/prv_month_orders,2) as percentage_change
 from (
-	   select *,
+	select *,
              lag(orders) over(order by month_number asc) prv_month_orders
        from cte
 	 ) x
@@ -57,17 +57,17 @@ from coffee_sales
 -- TOTAL QUANTITY SOLD KPI - MOM DIFFERENCE AND MOM GROWTH
 
 with cte as (
-				select to_char(transaction_date,'Mon') as months,extract(month from transaction_date ) as month_number
-					   ,sum(transaction_qty) as quantity
-				from coffee_sales
-				group by to_char(transaction_date,'Mon'),extract(month from transaction_date )
-				order by month_number asc
+	      select to_char(transaction_date,'Mon') as months,extract(month from transaction_date ) as month_number
+		     ,sum(transaction_qty) as quantity
+	      from coffee_sales
+	      group by to_char(transaction_date,'Mon'),extract(month from transaction_date )
+	      order by month_number asc
 	        )
 select months,quantity,prv_month_quantity,
        (quantity - prv_month_quantity) as diff_quantity,
 	   round((quantity - prv_month_quantity)*100.0/prv_month_quantity,2) as percentage_change
 from (
-	   select *,
+	select *,
              lag(quantity) over(order by month_number asc) prv_month_quantity
        from cte
 	 ) x
@@ -85,23 +85,23 @@ order by transaction_date
 -- COMPARISION OF AVERAGE SALES PER MONTH OVER PERIOD
 
 with cte as(
-			 select transaction_date, 
-				   round(sum(transaction_qty*unit_price),2) as sales,
-				   count(transaction_qty) as orders,
-				   sum(transaction_qty) as quantity
-			 from coffee_sales
-			 group by transaction_date
-			 order by transaction_date
+	     select transaction_date, 
+		     round(sum(transaction_qty*unit_price),2) as sales,
+		     count(transaction_qty) as orders,
+		     sum(transaction_qty) as quantity
+	     from coffee_sales
+	     group by transaction_date
+	     order by transaction_date
 	     )
 select *,
        round(avg(avg_sales) over(),2) as overall_avg
 from (
-		select to_char(transaction_date,'Mon') as month,
-			   extract(month from transaction_date) as month_number, 
-			   round(avg(sales),2) as avg_sales
-		from cte
-		group by to_char(transaction_date,'Mon'),extract(month from transaction_date)
-		order by month_number
+	select to_char(transaction_date,'Mon') as month,
+		extract(month from transaction_date) as month_number, 
+		round(avg(sales),2) as avg_sales
+	from cte
+	group by to_char(transaction_date,'Mon'),extract(month from transaction_date)
+	order by month_number
 	 ) x
 
 -- SALES BY WEEKDAY / WEEKEND OVER MONTHS
@@ -109,11 +109,11 @@ from (
 select * from coffee_sales
 
 with cte as (
-				select to_char(transaction_date,'Mon') as month,
-					   extract(month from transaction_date) as month_number,
-					   extract(DOW from transaction_date) as day_week_number,
-					  transaction_qty, unit_price
-				from coffee_sales
+	      select to_char(transaction_date,'Mon') as month,
+		     extract(month from transaction_date) as month_number,
+		     extract(DOW from transaction_date) as day_week_number,
+		     transaction_qty, unit_price
+	      from coffee_sales
 	        )
 select month,weekday,weekend
 from (
